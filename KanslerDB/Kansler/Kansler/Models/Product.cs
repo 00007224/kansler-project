f0b1a34953e17cf12a1c7851a39ca0b2a04c1bcd
Timbalan <55178421+Timbalan@users.noreply.github.com>
2021-04-14 16:40:16 +0500

    second part was done

--- KanslerDB/Kansler/Kansler/Models/Product.cs
+++ KanslerDB/Kansler/Kansler/Models/Product.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Web;
 
@@ -9,5 +10,25 @@ namespace Kansler.Models
     {
         public int ID { get; set; }
         public string Name { get; set; }
+        public /* image*/ int  Image { get; set; }
+        public string  Description { get; set; }
+        public int Category { get; set; }
+        public float Price { get; set; }
+        public int Barcode { get; set; }
+        public int Quantity { get; set; }
+        public int Warehouse { get; set; }
+
+        [DisplayName("Supllier Name")]
+        public int SupplierName { get; set; }
+
+        [DisplayName("Supplier Date")]
+        public DateTime SupplyDate { get; set; }
+
+        [DisplayName("Supplier Country")]
+        public int SupplierCountry { get; set; }
+
+        [DisplayName("Is in stock")]
+        public int IsInStock { get; set; }
     }
 }
+    
